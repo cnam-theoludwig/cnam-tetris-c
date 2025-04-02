3f790bc420b638982ea6e74fcf352b1f63ba9bbd
Théo LUDWIG <contact@theoludwig.fr>
2025-04-02 10:25:46 +0200

    feat: detect game over

--- lib/tetris_add_tetromino.h
+++ lib/tetris_add_tetromino.h
@@ -8,7 +8,14 @@
 #include "./tetris_struct.h"
 #include "./utils.h"
 
-void tetris_add_tetromino(struct Tetris* tetris, byte_t type);
+/**
+ * @brief
+ *
+ * @param tetris
+ * @param type
+ * @return bool true if the tetromino has space to be added, false otherwise.
+ */
+bool tetris_add_tetromino(struct Tetris* tetris, byte_t type);
 
 byte_t tetris_get_tetromino_random();
 
