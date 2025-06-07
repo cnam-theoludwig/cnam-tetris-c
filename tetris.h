844067a8ce50999e164d179fc6b19c67077469cc
Vincent PETT <pett.vincent@gmail.com>
2025-06-07 14:25:02 +0200

    fix: ci clang-format

--- tetris.h
+++ tetris.h
@@ -4,10 +4,10 @@
 #include "lib/tetris_add_tetromino.h"
 #include "lib/tetris_destroy.h"
 #include "lib/tetris_last_tetromino_step.h"
+#include "lib/tetris_main_menu.h"
 #include "lib/tetris_struct.h"
 #include "lib/tetris_ui.h"
-#include "lib/utils.h"
 #include "lib/tetris_ui_1v1.h"
-#include "lib/tetris_main_menu.h"
+#include "lib/utils.h"
 
 #endif
